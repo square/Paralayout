eb9317444364c26187198e48e489478465761b30
Nicholas Entin <entin@squareup.com>
2023-05-21 18:28:59 -0700

    Update CI builds off of macOS 10.15

--- Scripts/github/prepare-simulators.sh
+++ Scripts/github/prepare-simulators.sh
@@ -11,6 +11,14 @@ case $1 in
 	iOS_13)
 		sudo ln -s /Applications/Xcode_11.7.app/Contents/Developer/Platforms/iPhoneOS.platform/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS.simruntime /Library/Developer/CoreSimulator/Profiles/Runtimes/iOS\ 13.7.simruntime
 		;;
+
+	iOS_14)
+		sudo ln -s /Applications/Xcode_12.4.app/Contents/Developer/Platforms/iPhoneOS.platform/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS.simruntime /Library/Developer/CoreSimulator/Profiles/Runtimes/iOS\ 14.4.simruntime
+		;;
+
+	iOS_15)
+		sudo ln -s /Applications/Xcode_13.4.1.app/Contents/Developer/Platforms/iPhoneOS.platform/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS.simruntime /Library/Developer/CoreSimulator/Profiles/Runtimes/iOS\ 15.5.simruntime
+		;;
 esac
 
 xcrun simctl list runtimes
